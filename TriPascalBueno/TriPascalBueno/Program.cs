cbb07f640da9abc56ef9fa42e06255a923ab0b00
SERGIO GIL IGLESIAS <SERGIO GIL IGLESIAS>
2016-02-24 23:09:36 -0600

    primer programa en subir

---
+++ TriPascalBueno/TriPascalBueno/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriPascalBueno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int renTotal = 10;
+            int [] fil_Actual= new int[renTotal];
+            int[] fil_Anterior = new int[renTotal];
+            
+            //r = los renglones donde me encuentro
+            //hace el recorrido de de renglones (FILAS)
+            for (int r = 1; r <= renTotal; r++)
+            {
+                //imprime los espacios en blanco 
+                for (int nb = 1; nb <= renTotal - r; nb++)
+                {
+                    Console.Write(" ");                
+                }             
+                //el for que hace el recorrido
+                //de columnas 
+                    for (int col = 0; col < r; col++)
+                    {
+                        
+                        //condicion que imprime los #1
+                        if(col == 0 || col == r-1) 
+                        {
+                            fil_Actual[col]=1;
+                            Console.Write(fil_Actual[col] + " ");
+                            
+                                                                 
+                        }else//si no: hacer las operaciones 
+                        {
+                            fil_Actual[col] = fil_Anterior[col - 1] + fil_Anterior[col];
+                            Console.Write(fil_Actual[col] + " ");
+
+                            
+                        }
+                        
+                        
+                    }
+                    //se utiliza el metodo Utility para hacer la copia del arreglo 
+                    Utility.Copy(fil_Anterior, fil_Actual);
+
+                    
+                Console.WriteLine();
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TriPascalBueno/TriPascalBueno/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriPascalBueno
+{
+    class Utility
+    {
+        /*
+        static void Copy(int[] src, int[] dst)
+        {
+            for (int copi = 0; copi < src.Length; copi++)
+            {
+                dst[copi] =src[copi];
+            }
+        }
+
+        */
+        public static void Copy(int[] dst, int[] src)
+        {
+            //throw new NotImplementedException();
+            for (int copi = 0; copi < src.Length; copi++)
+            {
+               dst[copi] = src[copi];
+            }
+        }
+    }
+    
+        
+        
+}
